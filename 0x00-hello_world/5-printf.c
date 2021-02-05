3a58fd544658c1b2b9cc172b868b3596b679be63
ikuma-glitch <ikumamercy@gmail.com>
2021-02-05 07:59:08 +0000

    c-hello world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,7 @@
-#!/bin/bash
- int main () { printf("with proper grammar, but the outcome is a piece of art,"); return(0) }
+#include <stdio.h>
+ int main ()
+ {
+
+       printf("with proper grammar, but the outcome is a piece of art,\n"); 
+       return(0);
+ }
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("size of a char: %2d bytes \n", sizeof(char));
+  printf("size of an int: %2d bytes \n"
