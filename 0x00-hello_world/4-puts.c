f573fd93511a9bb994eed8b466c491556a613494
ikuma-glitch <ikumamercy@gmail.com>
2021-02-05 08:26:05 +0000

    C-hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-
+/*description*/
 int main ()
 {
-   const char string[] = "Programming is like building a multilingual puzzle";
-  puts (string);
+      puts("\"Programming is like building a multilingual puzzle");
      
       return(0);
 }
