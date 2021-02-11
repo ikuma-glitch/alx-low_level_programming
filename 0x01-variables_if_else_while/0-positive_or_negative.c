439beacc13a0f9ffb12a3842fb375d92e03cc013
ikuma-glitch <ikumamercy@gmail.com>
2021-02-11 09:40:02 +0000

    c variables

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,22 +6,17 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-         int n = 0;
+  int n;
 
-         if ( n > 0 ) {
-	   printf("is positive\n");
-	     }
-    
-         if ( n == 0) {
-           printf("is 0\n");
-	     }
-
-         if ( n < 0) {
-           printf("is negative\n");
-	     }
-
-         srand(time(0));
-         n = rand() - RAND_MAX / 2;
-         /*your code goes there */
-         return (0);
+  srand(time(0));
+  n = rand() - RAND_MAX / 2;
+  /*your code goes there */
+  if (n>0)
+    printf("is positive/n");
+  else if (n<0)
+    printf("is negative/n");
+  else 
+    printf("is zero/n");
+         
+  return (0);
 }
